6bba5550e4b7ca27f8347e710c712a988e88409a
ari <ari@ari.ru>
2024-03-07 14:34:41 +1000

    localStorage task

--- src/stores/visitCounter.js
+++ src/stores/visitCounter.js
@@ -2,26 +2,44 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('visitCounter', () => {
-  const viewData = ref({
-    whiteBoxes: {
-      count: 0,
-      date: null
-    },
-    counter: {
-      count: 0,
-      date: null
-    },
-    slider: {
-      count: 0,
-      date: null
-    },
-  });
+  let initialData;
+
+  if (localStorage.getItem("metricsData")) {
+    initialData = JSON.parse(localStorage.getItem("metricsData"));
+  } else {
+    initialData = {
+      whiteBoxes: {
+        count: 0,
+        date: null
+      },
+      counter: {
+        count: 0,
+        date: null
+      },
+      slider: {
+        count: 0,
+        date: null
+      },
+    }
+  }
+  console.log(initialData);
+  const viewData = ref({initialData});
 
   
 
   function increaseComponentView(componentName) {
+
+    // if (!localStorage.getItem("metricsData")) {
+    //   viewData.value[componentName].count = 0;
+    //   viewData.value[componentName].date = null;
+
+      
+    // }  
+    
     viewData.value[componentName].count++;
     viewData.value[componentName].date = new Date();
+
+    window.localStorage.setItem('metricsData', JSON.stringify(viewData));
   }
 
   return { viewData, increaseComponentView }
